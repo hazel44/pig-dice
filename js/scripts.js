42f2b5aab279bb063ffcc56e540fb09de9d6a2d8
hazel44 <miriamhazel44@gmail.com>
2019-03-05 09:39:55 +0300

    working on business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-//Business logic
+// Business logic
+
+function PigDicePlayer (name, winningscore) {
+    this.player= name;
+    this.totalscore = winningscore;
+    this.currentroll = currentroll
+    this.currentscore= currentscore
+} 
+
+PigDicePlayer.prototype.rollsdice = function(){
+    return this.player + " " + this.currentroll +" " + this.currentscore + " " + this.totalscore;
+
+}
+ // Dice rolles  Numbers between 1 and 6
+
+  var diceRolled = Math.floor(Math.random()*6)+1;
+
+  if (diceRolled >1 ){
+      this.currentroll += diceRolled;
+      this.currentscore += 1;
+      //return dicerolled;
+      }
+      else if(diceRolled === 1){
+          this.currentroll = 0;
+          //return 0;
+      }
+
+
+// User interface logic
+
+$(document).ready(function(){
+    $("#play").click(function(event){
+        event.preventDefault();
+
+// Display value after dice is rolled for player 1
+$("#play #player1rolls").click(function(){
+    $("#col").text(diceRolled)
+})
+
+    });
+
+});
